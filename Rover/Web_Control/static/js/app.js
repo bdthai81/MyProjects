f16d5fd70b50da9de7ff28297230a6b084e03118
bdthai81 <bdthai81@gmail.com>
2019-03-04 02:20:27 -0800

    add Web Control: complete remote control template

---
+++ Rover/Web_Control/static/js/app.js
@@ -0,0 +1,9 @@
+var buttonSVG = document.querySelector('.button-svg');
+var forward = document.querySelector("#forward");
+var reverse = document.querySelector("#reverse");
+var left = document.querySelector("#left");
+var right = document.querySelector("#right");
+
+forward.onclick = function(){
+    console.log(this);
+}
